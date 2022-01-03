d557afcfb5a9ad0c5aa551003d44433c561871c4
Amalia Contiero <amycontiero@gmail.com>
2022-01-03 22:53:47 +0200

    check if palindrome 3/1/22

---
+++ Text/Check if palindrome/check_if_palindrome.py
@@ -0,0 +1,9 @@
+word = input("Enter a word:")
+reverse_word = word[::-1]
+
+if (word == reverse_word):
+    print("Palindrome")
+else:
+    print("Not palindrome")
+ 
+exit()
--- Text/Reverse String/reverse_string.py
+++ Text/Reverse String/reverse_string.py
@@ -1,3 +1,5 @@
 phrase = input("Enter a phrase:")
 reverse_phrase = phrase[::-1]
 print(reverse_phrase)
+
+exit()
